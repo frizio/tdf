4a947abbfc31cf2f24c925111eed90eb81e2e6df
Maurizio La Rocca <mrz.larocca@gmail.com>
2018-06-28 12:10:33 +0200

    6 - Binding Data to a Model (1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+
   title = 'Bootcamp Enrollment Form';
 
   topics = ['Angular', 'React', 'Vue'];
+
+  // Instance of the model
+  userModel = new User('Max', 'max@gmail.com', 390918983067, '', 'morning', true);
+
+  // Bind the user model to the enrollment form using interpolation
 
 }
